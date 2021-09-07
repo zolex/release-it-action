1fcfb80b2685df7860adc2379ef36a0dc0b0f7d6
Andreas Linden <andreas.linden@coxautoinc.com>
2021-09-07 12:25:34 +0200

    set version output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-echo "Github Token: $1"
 export GITHUB_TOKEN="$1"
 
 ls -lah $GITHUB_WORKSPACE
@@ -9,4 +8,7 @@ cd $GITHUB_WORKSPACE
 git config --global user.name "$3"
 git config --global user.email "$4"
 
+export RELEASE_VERSION=$(release-it --release-version | tail -n1)
 release-it -VV --ci
+
+echo "::set-output name=version::$RELEASE_VERSION"
