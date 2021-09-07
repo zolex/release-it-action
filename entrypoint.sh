c6f4fd07f60161237f2d4fcb51423509efe96311
Andreas Linden <andreas.linden@coxautoinc.com>
2021-09-07 15:00:43 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
-export GITHUB_TOKEN="$1"
-
-ls -lah $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
+ls -lah .
 
 git config --global user.name "$3"
 git config --global user.email "$4"
+echo "Git user $3<$4>"
 
+export GITHUB_TOKEN=$1
 export RELEASE_VERSION=$(release-it --release-version | tail -n1)
+echo "Bump version to $RELEASE_VERSION"
+
 release-it -VV --ci || exit $?
 
 echo "::set-output name=version::$RELEASE_VERSION"
