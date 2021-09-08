1875a690dfa6c718ddf04df4cec8ae573688a904
Andreas Linden <andreas.linden@coxautoinc.com>
2021-09-08 08:48:34 +0200

    add pre-release feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -l
 
 cd $GITHUB_WORKSPACE
-ls -lah .
 
-git config --global user.name "$3"
-git config --global user.email "$4"
-echo "Git user $3<$4>"
+git config user.name "$3"
+git config user.email "$4"
 
 export GITHUB_TOKEN=$1
-export RELEASE_VERSION=$(release-it --release-version | tail -n1)
-echo "Bump version to $RELEASE_VERSION"
+if [ "$5" != "" ]; then
+  PRE_RELEASE="--preRelease=$5 --github.preRelease"
+fi
+RELEASE_VERSION=$(release-it --release-version $PRE_RELEASE | tail -n1)
+echo "New version: $RELEASE_VERSION"
 
-release-it -VV --ci || exit $?
+release-it -VV --ci $PRE_RELEASE || exit $?
 
 echo "::set-output name=version::$RELEASE_VERSION"
